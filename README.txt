2ce80d2afe51b6a46e491e13f3919b0a1e36b76a
unknown <tstarrs@mail.umw.edu>
2015-01-31 14:28:17 -0500

    Adding base files

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is a Reddit Bot that functions as a dictionary. It is called by the
+phrase "DefineMe!" and will respond to the word following that phrase with its
+definition and a link to it'`s Merriam Webster Dictionary page. 
---
+++ RedditBot.py
@@ -0,0 +1,47 @@
+#This is a dictionary bot using the Merriam Webster API by /u/tstarrs
+#Credit goes to pfeyz on Github for the API wrapper functions used by this bot. 
+
+
+import praw
+import os
+import sys
+import re
+import time
+from define import lookup
+from api import(CollegiateDictionary, WordNotFoundException)
+
+API_KEY = open("API_KEY.txt", 'r').read().rstrip()
+os.environ['MERRIAM_WEBSTER_COLLEGIATE_KEY'] = API_KEY
+
+#Acquire login info and Login
+user_agent = open("User_Agent.txt", 'r').read().rstrip()
+bot_username = open("username.txt", 'r').read().rstrip()
+bot_password = open("password.txt", "r").read().rstrip()
+r = praw.Reddit(user_agent=user_agent)
+r.login(username = bot_username, password = bot_password)
+print("Logged in")
+already_done = []
+
+#get Merriam Webster API Key
+collkey = os.getenv("MERRIAM_WEBSTER_COLLEGIATE_KEY")
+
+#keyWord = "DefineMe!"
+while True:
+	subreddit = r.get_subreddit('UMW_CPSC470Z')
+	subreddit_comments = subreddit.get_comments()
+	for comment in subreddit_comments:
+		#has_keyWord = any(string in comment.body for keyWord)
+		if comment.id not in already_done:
+			if re.search("DefineMe! ", comment.body):
+				#get word from comment
+				word = re.search("DefineMe! (.*)", comment.body, re.IGNORECASE).groups()
+				query = word[0]
+				lookupResult = lookup(CollegiateDictionary, collkey, query)
+				REPLY = lookupResult
+				print REPLY
+				comment.reply(REPLY)
+				print("Should have replied")
+				already_done.append(comment.id)
+
+	time.sleep(1800)
+
