3278272f1558d359b03cab5bd56c128920721257
Cloud-Rush <tstarrs@mail.umw.edu>
2015-01-31 17:00:02 -0500

    Update define.py

--- define.py
+++ define.py
@@ -1,55 +1,8 @@
-# -*- encoding: utf-8 -*-
-
-"""
-Simple command-line tool that fetches the dictionary definitions of a given word
-from Merriam Webster.
-
-  $ python define.py bike
-  LEARNERS: bike [noun]: bicycle
-  LEARNERS: bike [noun]: motorcycle
-  LEARNERS: bike [verb]: to ride a bicycle
-  COLLEGIATE: bike [noun]: a nest of wild bees, wasps, or hornets
-  COLLEGIATE: bike [noun]: a crowd or swarm of people
-  COLLEGIATE: bike [noun]: bicycle
-  COLLEGIATE: bike [noun]: motorcycle
-  COLLEGIATE: bike [noun]: motorbike
-  COLLEGIATE: bike [noun]: stationary bicycle
-  COLLEGIATE: bike [verb]: to ride a bike
-
-  $ python define.py definition
-  LEARNERS: definition [noun]: an explanation of the meaning of a word, phrase, etc.; a statement that defines a word, phrase, etc.
-  LEARNERS: definition [noun]: a statement that describes what something is
-  LEARNERS: definition [noun]: a clear or perfect example of a person or thing
-  LEARNERS: definition [noun]: the quality that makes it possible to see the shape, outline, and details of something clearly
-  COLLEGIATE: definition [noun]: an act of determining
-  COLLEGIATE: definition [noun]: the formal proclamation of a Roman Catholic dogma
-  COLLEGIATE: definition [noun]: a statement expressing the essential nature of something
-  COLLEGIATE: definition [noun]: a statement of the meaning of a word or word group or a sign or symbol
-  COLLEGIATE: definition [noun]: a product of defining
-  COLLEGIATE: definition [noun]: the action or process of stating the meaning of a word or word group
-  COLLEGIATE: definition [noun]: the action or the power of describing, explaining, or making definite and clear
-  COLLEGIATE: definition [noun]: clarity of visual presentation; distinctness of outline or detail
-  COLLEGIATE: definition [noun]: clarity especially of musical sound in reproduction
-  COLLEGIATE: definition [noun]: sharp demarcation of outlines or limits
-  COLLEGIATE: definition [adjective]: being or relating to an often digital television system that has twice as many scan lines per frame as a conventional system, a proportionally sharper image, and a wide-screen format
-  COLLEGIATE: definition [noun]: the evaluation by oneself of one's worth as an individual in distinction from one's interpersonal or social roles
-
-
-  $ python define.py refactor
-  LEARNERS: No definitions found for 'refactor'
-  COLLEGIATE: No definitions found for 'refactor'
-
-"""
-
 import os
 import sys
 
 from api import (LearnersDictionary, CollegiateDictionary,
                                  WordNotFoundException)
-
-os.environ['MERRIAM_WEBSTER_COLLEGIATE_KEY'] = "8a6a28c0-9617-42cc-b5c0-37e8f920e409"
-
-
 def lookup(dictionary_class, key, query):
     dictionary = dictionary_class(key)
     string = ""
@@ -59,13 +12,12 @@ def lookup(dictionary_class, key, query):
                 for definition, examples in entry.senses]
     except WordNotFoundException:
         defs = []
-    dname = dictionary_class.__name__.replace('Dictionary', '').upper()
     if defs == []:
-        return "No definitions found for '{1}'".format(dname, query)
+        return "I couldn't find a definition for: '{0}'".format(query)
     for word, pos, definition in defs:
-        string +=  "{1} [{2}]: {3}\n\n".format(dname, word, pos, definition)
+        string +=  "{0} [{1}]: {2}\n\n".format(word, pos, definition)
         #print string
-    string += "http://www.merriam-webster.com/dictionary/{1}".format(dname, word, pos, definition)
+    string += "http://www.merriam-webster.com/dictionary/{0}".format(word, pos, definition)
     return string
 
 
