1ac0e5e428459b3a615837fc90f65f0e6c6c800c
Thomas Starrs <tstarrs@mail.umw.edu>
2015-01-31 22:29:13 +0000

    pushing bot changes

--- RedditBot.py
+++ RedditBot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #This is a dictionary bot using the Merriam Webster API by /u/tstarrs
 #Credit goes to pfeyz on Github for the API wrapper functions used by this bot. 
 
@@ -10,7 +12,7 @@ import time
 from define import lookup
 from api import(CollegiateDictionary, WordNotFoundException)
 
-API_KEY = open("API_KEY.txt", 'r').read().rstrip()
+API_KEY = open("API_Key.txt", 'r').read().rstrip()
 os.environ['MERRIAM_WEBSTER_COLLEGIATE_KEY'] = API_KEY
 
 #Acquire login info and Login
@@ -20,7 +22,15 @@ bot_password = open("password.txt", "r").read().rstrip()
 r = praw.Reddit(user_agent=user_agent)
 r.login(username = bot_username, password = bot_password)
 print("Logged in")
-already_done = []
+
+if not os.path.isfile("readComments.txt"):
+	replies = []
+else:
+	print "Loading previous replies"
+	with open("readComments.txt", "r") as f:
+		replies = f.read()
+		replies = replies.split("\n")
+		replies = filter(None, replies)
 
 #get Merriam Webster API Key
 collkey = os.getenv("MERRIAM_WEBSTER_COLLEGIATE_KEY")
@@ -31,7 +41,7 @@ while True:
 	subreddit_comments = subreddit.get_comments()
 	for comment in subreddit_comments:
 		#has_keyWord = any(string in comment.body for keyWord)
-		if comment.id not in already_done:
+		if comment.id not in replies:
 			if re.search("DefineMe! ", comment.body):
 				#get word from comment
 				word = re.search("DefineMe! (.*)", comment.body, re.IGNORECASE).groups()
@@ -41,7 +51,11 @@ while True:
 				print REPLY
 				comment.reply(REPLY)
 				print("Should have replied")
-				already_done.append(comment.id)
-
-	time.sleep(1800)
+				replies.append(comment.id)
+	print("Saving new ids to file")
+	with open("readComments.txt", "w") as f:
+		for i in replies:
+			f.write(i + "\n")
+	print("sleeping!")
+	time.sleep(600)
 
